784e45cff2b52fddf2abefd809b49593d5504862
Mohammed <mo7amxd.7@gmail.com>
2024-06-22 23:15:25 +0100

    2.0

--- app/src/main/java/com/example/introtostateincompose/MainActivity.kt
+++ app/src/main/java/com/example/introtostateincompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.introtostateincompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,19 +12,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.introtostateincompose.ui.theme.IntroToStateInComposeTheme
 import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
@@ -52,7 +55,7 @@ fun TipTimeLayout(modifier: Modifier = Modifier) {
                 .align(Alignment.Start)
         )
         EditNumberField(
-            textBox = "",
+
             modifier = modifier
                 .fillMaxWidth()
                 .padding(bottom = 32.dp)
@@ -76,15 +79,26 @@ private fun calculateTip(amount: Double, tipPercentage: Double = 15.0): String {
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
-fun EditNumberField(textBox: String, modifier: Modifier = Modifier) {
-    val amountInput = "0"
+fun EditNumberField( modifier: Modifier = Modifier) {
+
+    var amountInput by remember {
+
+        mutableStateOf("")
+    }
     TextField(
-        value = "",
-        onValueChange = {},
-        modifier = modifier
+        value = amountInput,
+        onValueChange = { amountInput = it },
+        label = {
+            Text(text = stringResource(R.string.amount))
+        },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        ,modifier = modifier
     )
 }
+
 
 @Preview(showBackground = true)
 @Composable
