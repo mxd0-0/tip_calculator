49713d9478606ac558d2a3edb25a3252fe5533e5
Mohammed <mo7amxd.7@gmail.com>
2024-07-08 23:22:58 +0100

    Test App

--- app/src/test/java/com/example/introtostateincompose/TipCalculatorTest.kt
+++ app/src/test/java/com/example/introtostateincompose/TipCalculatorTest.kt
@@ -1,4 +1,12 @@
 package com.example.introtostateincompose
 
+import org.junit.Test
+
 class TipCalculatorTest {
+    @Test
+    fun calculateTip_20PercentNoRoundup() {
+        val amount = 10.00
+        val tipPecent = 20.00
+
+    }
 }
